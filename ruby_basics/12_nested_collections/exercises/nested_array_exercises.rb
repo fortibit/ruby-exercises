675cdfeb2077930140e6f825cf94981c483896b2
fortibit <robdomu@gmail.com>
2026-05-14 20:29:12 +0200

    Add solution

--- ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
@@ -48,6 +48,7 @@ def count_empty_seats(chart)
   # take a chart and return the number of empty (nil) seats in it
 
   # NOTE: `chart` should **not** be mutated
+  chart.flatten.select { |seat| seat == nil }.length
 end
 
 def find_favorite(array_of_hash_objects)
